e5d59fcb545f16379a1b68542905b7de5ca3a252
Unixity <103908889+Unixityyy@users.noreply.github.com>
2025-12-28 15:22:00 -0800

    electron (try 1)
    i hope this works first try tbh

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: __dirname + '/favicon.ico',
     webPreferences: { nodeIntegration: true }
   });
 
